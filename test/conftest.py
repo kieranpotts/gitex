211305cc166c4e7db9b77bc1c628627d57f4d3f6
Kieran Potts <kieranpotts@users.noreply.github.com>
2025-11-04 07:28:35 +0000

    dev: establish initial tests

---
+++ test/conftest.py
@@ -0,0 +1,16 @@
+# Sharing fixtures
+# Ref: https://docs.pytest.org/en/6.2.x/fixture.html#scope-sharing-fixtures-across-classes-modules-packages-or-session
+
+import pytest
+
+from helper import TempRepo
+
+
+@pytest.fixture(scope="function")
+def temp_repo():
+    """Create a temporary Git repository that is reset for each test 
+    function call."""
+
+    repo = TempRepo()
+    
+    return repo
---
+++ test/helper.py
@@ -0,0 +1,43 @@
+"""
+Helper functions for tests.
+"""
+
+import os
+import tempfile
+
+# https://github.com/gitpython-developers/GitPython
+# https://gitpython.readthedocs.io/en/stable/tutorial.html
+from git import Repo
+
+
+class TempRepo:
+    """Class to create and manage a temporary Git repository for testing purposes."""
+
+
+    def __init__(self):
+        self._cwd = tempfile.mkdtemp()
+        self._repo = Repo.init(self._cwd, b="main")
+        self._files = []
+
+        self.cd_repo()
+
+
+    def cd_repo(self):
+        os.chdir(self._cwd)
+        print(f"The current work directory has switched to {self._cwd}")
+
+
+    def cwd(self):
+        return self._cwd
+
+
+    def dirname(self):
+        """Get the directory name of the temporary repository"""
+
+        system_tmpdir = tempfile.gettempdir()
+    
+        return self._cwd[len(system_tmpdir) + 1 :]
+
+
+    def git(self):
+        return self._repo.git
